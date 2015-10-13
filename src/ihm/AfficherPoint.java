b0bd0f457037e39becd34fac96ab7c42ddc17613
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2015-10-13 16:45:21 +0200

    norme

--- src/ihm/AfficherPoint.java
+++ src/ihm/AfficherPoint.java
@@ -8,28 +8,26 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class AfficherPoint extends JPanel {
-	/** Debug **/
-	boolean debug = true;	
-	
-	Point coord;
-	public AfficherPoint(Graphics g,Point coord,Color couleur) 
-	{
-		int taille = 5;
-		super.paint(g);
-		Color c = g.getColor();
-		this.coord=coord;
-		g.setColor(couleur);
-		g.fillOval(coord.x-(taille/2),coord.y-(taille/2),taille,taille);
-		g.setColor(c);
-	}
-	
-	public int getX()
-	{
-		return this.coord.x;
-	}
-	
-	public int getY()
-	{
-		return this.coord.y;
-	}
+  /** Debug **/
+  boolean debug = true;
+
+  Point coord;
+
+  public AfficherPoint(Graphics g, Point coord, Color couleur) {
+    int taille = 5;
+    super.paint(g);
+    Color c = g.getColor();
+    this.coord = coord;
+    g.setColor(couleur);
+    g.fillOval(coord.x - (taille / 2), coord.y - (taille / 2), taille, taille);
+    g.setColor(c);
+  }
+
+  public int getX() {
+    return this.coord.x;
+  }
+
+  public int getY() {
+    return this.coord.y;
+  }
 }

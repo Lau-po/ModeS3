4ba962ce80a1d1db4ab9e7ca8a566e6d989eec02
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2015-10-13 16:45:56 +0200

    norme + affichage centré et courbe normalisé

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -8,28 +8,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import technique.Calcul;
+import technique.Obstacle;
 
 public class Main {
-	public static void main(String[] args) {
-		/** Création des variables **/
-		//int hauteur = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-		//int largeur  = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-		
-		int hauteur = 500;
-		int largeur = 500;
-		boolean debug = true;
-		
-		Affichage test = new Affichage(largeur,hauteur);
-		
-		List<Point> list_pdc = Calcul.createPointCtrl(largeur, hauteur);
-		List<Point> courbe = new ArrayList<Point>();
-		for(float i = 0; i <= 1; i+=0.01) {
-			courbe.add(Calcul.creerCourbe(list_pdc, i));
-		}
-		if(debug) {
-			test.afficheCourbe(list_pdc,Color.black);
-		}
-		test.afficheCourbe(courbe,Color.red);
-		
-	}
+  public static void main(String[] args) {
+    /** Création des variables **/
+    // int hauteur = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    // int largeur = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+
+    int hauteur = 500;
+    int largeur = 500;
+    boolean debug = true;
+
+    Affichage test = new Affichage(largeur, hauteur);
+    Calcul calcul = new Calcul(largeur, hauteur);
+    List<Point> list_pdc = calcul.createPointCtrl();
+    List<Point> courbe = new ArrayList<Point>();
+    List<Obstacle> obstacles = new ArrayList<Obstacle>();
+    
+    for (float i = 0; i <= 1; i += 0.01) {
+      courbe.add(calcul.creerCourbe(list_pdc, i));
+    }
+    if (debug) {
+      test.afficheCourbe(list_pdc, Color.black);
+    }
+    test.afficheCourbe(courbe, Color.red);
+
+  }
 }
--- src/technique/Calcul.java
+++ src/technique/Calcul.java
@@ -6,40 +6,50 @@ import java.util.List;
 import java.util.Random;
 
 public class Calcul {
-	/** Debug **/
-	static boolean debug = true;
-	/**
-	 * 
-	 * @param list_pdc = liste de points de contrôles
-	 * @param t = instant d'un temps donné
-	 * @return point de la courbe à instant 't'
-	 */
-	public static Point creerCourbe(List<Point> list_pdc, float t) {
-		ArrayList<Point> list_pv = new ArrayList<Point>();
-		if(list_pdc.size() -1 == 0) {
-			return list_pdc.get(0);
-		}
-		for(int i = 0; i < list_pdc.size()-1; i++) {
-			int x=(int) ((1-t)*list_pdc.get(i).getX() + t*list_pdc.get(i+1).getX());
-			int y=(int) ((1-t)*list_pdc.get(i).getY() + t*list_pdc.get(i+1).getY());
-			list_pv.add(new Point(x,y));
-		}
-		return creerCourbe(list_pv,t);
-	}
-	
-	/**
-	 * Fonction qui place les points de contrôles d'une courbe de Bézier
-	 */
-	public static List<Point> createPointCtrl(int largeur, int hauteur) {
-		List<Point> list_pdc = new ArrayList<Point>();
-		Random r = new Random(); 
-		list_pdc.add(new Point(10,largeur-10));
-		list_pdc.add(new Point((r.nextInt(largeur)),r.nextInt(hauteur)));
-		list_pdc.add(new Point((r.nextInt(largeur)),r.nextInt(hauteur)));
-		//TODO
+  
+  private int l;
+  private int h;
+  
+  /** Debug **/
+  static boolean debug = true;
 
-		return list_pdc;
-		
-		
-	}
+  public Calcul(int l, int h) {
+    this.l = l;
+    this.h = h;
+  }
+  
+  /**
+   * 
+   * @param list_pdc = liste de points de contrôles
+   * @param t = instant d'un temps donné
+   * @return point de la courbe à instant 't'
+   */
+  public Point creerCourbe(List<Point> list_pdc, float t) {
+    ArrayList<Point> list_pv = new ArrayList<Point>();
+    if (list_pdc.size() - 1 == 0) {
+      return list_pdc.get(0);
+    }
+    for (int i = 0; i < list_pdc.size() - 1; i++) {
+      int x = (int) ((1 - t) * list_pdc.get(i).getX() + t * list_pdc.get(i + 1).getX());
+      int y = (int) ((1 - t) * list_pdc.get(i).getY() + t * list_pdc.get(i + 1).getY());
+      list_pv.add(new Point(x, y));
+    }
+    return creerCourbe(list_pv, t);
+  }
+
+  /**
+   * Fonction qui place les points de contrôles d'une courbe de Bézier
+   */
+  public List<Point> createPointCtrl() {
+    List<Point> list_pdc = new ArrayList<Point>();
+    Random r = new Random();
+    list_pdc.add(new Point(10, this.l - 10));
+    list_pdc.add(new Point((r.nextInt(20)+50), r.nextInt(20)+120));
+    list_pdc.add(new Point((r.nextInt(50)+this.l+this.l/2), r.nextInt(50)+this.h/2));
+    // TODO
+
+    return list_pdc;
+
+
+  }
 }

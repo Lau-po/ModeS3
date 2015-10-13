54821710c45e32350e6cc1e6911e3930ae1cc81c
Clement DUSART <clement.dusart@etudiant.univ-lille1.fr>
2015-10-13 16:45:01 +0200

    obstacles

---
+++ src/technique/Obstacle.java
@@ -0,0 +1,32 @@
+package technique;
+
+import java.awt.Color;
+import java.awt.Point;
+
+public class Obstacle extends Point {
+
+  public static final Color COLOR = Color.blue;
+  private boolean touched;
+
+  public Obstacle(int x, int y) {
+    super(x, y);
+    this.touched = false;
+  }
+
+  public boolean isTouched() {
+    return touched;
+  }
+
+  public void setTouched(boolean touched) {
+    this.touched = touched;
+  }
+  
+  public double getX() {
+    return super.getX();
+  }
+  
+  public double getY() {
+    return super.getY();
+  }
+
+}

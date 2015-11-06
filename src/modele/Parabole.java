2182938a77757c6a5418b0c169538cc9c1d8b0cb
RST <rudy.16@hotmail.fr>
2015-11-06 18:06:15 +0100

    livrable 1 + climping

---
+++ src/modele/Parabole.java
@@ -0,0 +1,79 @@
+/**
+ * Classe qui créer une parabole de test
+ * @author Groupe N5
+ */
+
+package modele;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Parabole extends ObservableModele {
+
+  /** Nombre de pas de la courbe*/
+  private double pas = 0.001;
+
+  /**
+   * Constructeur de base (depend pour l'instant d'une fenetre de 900*470)
+   */
+  public Parabole() {
+    super();
+    genObstacles();
+  }
+
+  /**
+   * Construit une parabole avec un pas de p 
+   * @param pas de la parabole
+   */
+  public Parabole(double pas) {
+    this();
+    this.pas = pas;
+  }
+
+  /**
+   * @see Modele
+   */
+  @Override
+  public void go() {
+    double y;
+    for (float x = 0; x < 970 && !collision; x += pas) {
+      y = (37.0 / 20250.0) * (x * x) + (-74.0 / 45.0) * x + 470;
+      courbe.add(new Point((int) x, (int) y));
+      setChanged();
+      notifyObservers();
+    }
+    if (collision) {
+      try {
+        Thread.sleep(1500);
+      } catch (InterruptedException e) {
+  		e.printStackTrace();
+  	} finally {
+        collision = false;
+      }
+    }
+  }
+  /**
+   * Fonction qui génére les obstacles
+   */
+  void genObstacles() {
+    super.genObstacles();
+  }
+
+  /**
+   * @see Modele
+   */
+  @Override
+  public void reset() {
+    courbe = new ArrayList<>();
+    obstacles = new ArrayList<>();
+    genObstacles();
+  }
+
+  /**
+   * @see Modele
+   */
+  @Override
+  public Piaf getPiaf() {
+	  return oiseau;
+  }
+}
---
+++ src/modele/Piaf.java
@@ -0,0 +1,96 @@
+/**
+ * Classe qui gère l'oiseau et son comportement
+ * @author Groupe N5
+ */
+
+package modele;
+
+import java.awt.Color;
+import java.awt.Point;
+
+@SuppressWarnings("serial")
+public class Piaf extends Point {
+	/** Bec lié à l'oiseau*/
+	private Bec bec;
+	/** Si l'oiseau est touché ou non*/
+	private boolean touched;
+	/** La couleur de l'oiseau*/
+	private Color c = Color.red;
+
+	/**
+	 * Constructeur de base
+	 * @param x  coordonnee verticale de l'oiseau
+	 * @param y  coordonnee horizontale de l'oiseau
+	 * @param pointe_bec de l'oiseau
+	 */
+	public Piaf(int x, int y,Point pointe_bec) {
+		super(x, y);
+		this.bec = new Bec(this, pointe_bec);
+		this.touched = false;
+	}
+	
+	/**
+	 * Fonction qui gère le mouvement de l'oiseau
+	 * @param x coordonnee verticale de l'oiseau
+	 * @param y coordonnee horizontale de l'oiseau
+	 * @param pointe_bec de l'oiseau
+	 */
+	public void move(int x, int y,Point pointe_bec)
+	{
+		super.move(x, y);
+		bec.setPointe(pointe_bec);
+	}
+	
+	/**
+	 * Fonction qui détermine si l'oiseau est touché
+	 * @return si l'oiseau est touche ou non
+	 */
+	public boolean isTouched() {
+		return touched;
+	}
+
+	/**
+	 * Fonction qui indique si l'oiseau est touché
+	 * @param touched ou non
+	 */
+	public void setTouched(boolean touched) {
+		this.touched = touched;
+		if (touched) {
+			c = Color.YELLOW;
+		} else {
+			c = Color.green;
+		}
+	}
+
+	/**
+	 * @see modele.Modele
+	 */
+	@Override
+	public double getX() {
+		return super.getX();
+	}
+
+	/**
+	 * @see modele.Modele
+	 */
+	@Override
+	public double getY() {
+		return super.getY();
+	}
+
+	/**
+	 * Fonction pour obtenir la couleur de l'oiseau
+	 * @return la couleur de l'oiseau
+	 */
+	public Color getC() {
+		return c;
+	}
+
+	/**
+	 * Fonction qui obtient le bec de l'oiseau
+	 * @return
+	 */
+	public Bec getBec() {
+		return bec;
+	}
+}

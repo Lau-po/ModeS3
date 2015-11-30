179d3509d3bea75fcb1db1dbed7c67baa27ccc20
RST <rudy.16@hotmail.fr>
2015-11-30 11:51:04 +0100

    debut obstacles qui bougent

--- src/modele/Bezier.java
+++ src/modele/Bezier.java
@@ -13,106 +13,116 @@ import java.util.Random;
 
 public class Bezier extends ObservableModele {
 
-  /** Liste des points de contrôle de la courbe */
-  private List<Point> ctrl;
-  /** Le nombre de points pour la courbe */
-  private double pas = 0.001;
+	/** Liste des points de contrôle de la courbe */
+	private List<Point> ctrl;
+	/** Le nombre de points pour la courbe */
+	private double pas = 0.001;
 
-  /**
-   * Constructeur de base
-   */
-  public Bezier() {
-    super();
-    createPointCtrl();
-    genObstacles();
-  }
+	/**
+	 * Constructeur de base
+	 */
+	public Bezier() {
+		super();
+		createPointCtrl();
+		genObstacles();
+	}
 
-  /**
-   * Construit une courbe de Bezier avec un pas donne
-   * 
-   * @param pas de la courbe
-   */
-  public Bezier(double pas) {
-    this();
-    this.pas = pas;
-  }
+	/**
+	 * Construit une courbe de Bezier avec un pas donne
+	 * 
+	 * @param pas
+	 *            de la courbe
+	 */
+	public Bezier(double pas) {
+		this();
+		this.pas = pas;
+	}
 
-  /**
-   * @see Modele
-   */
-  @Override
-  public void go() {
-    for (float i = 0; i <= 1 && !collision; i += pas) {
-      courbe.add(createCurve(ctrl, i));
-      oiseau.move(createCurve(ctrl, i).x, createCurve(ctrl, i).y,
-          createCurve(ctrl, (float) (i + (10 * pas))));
-      setChanged();
-      notifyObservers();
-      try {
-        Thread.sleep(5);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-    if (collision) {
-      try {
-        Thread.sleep(1500);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      } finally {
-        collision = false;
-      }
-    }
-  }
+	/**
+	 * @see Modele
+	 */
+	@Override
+	public void go() {
+		for (float i = 0; i <= 1 && !collision; i += pas) {
+			courbe.add(createCurve(ctrl, i));
+			oiseau.move(createCurve(ctrl, i).x, createCurve(ctrl, i).y,
+					createCurve(ctrl, (float) (i + (10 * pas))));
+			for (Obstacle o : obstacles) 
+			{
+				o.move();
 
-  /**
-   * Fonction qui gere la creation des points de contrôle
-   */
-  private void createPointCtrl() {
-    ctrl = new ArrayList<Point>();
-    Random r = new Random();
-    ctrl.add(new Point(10, 490));
-    ctrl.add(new Point((r.nextInt(20) + 50), r.nextInt(20) + 120));
-    ctrl.add(new Point((r.nextInt(20) + 500), r.nextInt(20) + 120));
-    ctrl.add(new Point(900, r.nextInt(150) + 150));
-  }
+			}
+			setChanged();
+			notifyObservers();
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		if (collision) {
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				collision = false;
+			}
+		}
+	}
 
-  /**
-   * Fonction qui gere la creation de la courbe de Bezier
-   * 
-   * @param list_pdc necessaire pour la courbe
-   * @param t = le pas
-   * @return un point de la courbe à l'instant t
-   */
-  private Point createCurve(List<Point> list_pdc, float t) {
-    ArrayList<Point> list_pv = new ArrayList<Point>();
-    if (list_pdc.size() - 1 == 0) {
-      return list_pdc.get(0);
-    }
-    for (int i = 0; i < list_pdc.size() - 1; i++) {
-      int x = (int) ((1 - t) * list_pdc.get(i).getX() + t * list_pdc.get(i + 1).getX());
-      int y = (int) ((1 - t) * list_pdc.get(i).getY() + t * list_pdc.get(i + 1).getY());
-      list_pv.add(new Point(x, y));
-    }
-    return createCurve(list_pv, t);
-  }
+	/**
+	 * Fonction qui gere la creation des points de contrôle
+	 */
+	private void createPointCtrl() {
+		ctrl = new ArrayList<Point>();
+		Random r = new Random();
+		ctrl.add(new Point(10, 490));
+		ctrl.add(new Point((r.nextInt(20) + 50), r.nextInt(20) + 120));
+		ctrl.add(new Point((r.nextInt(20) + 500), r.nextInt(20) + 120));
+		ctrl.add(new Point(900, r.nextInt(150) + 150));
+	}
 
-  /**
-   * Fonction qui genere les obstacles
-   */
-  void genObstacles() {
-    super.genObstacles();
-  }
+	/**
+	 * Fonction qui gere la creation de la courbe de Bezier
+	 * 
+	 * @param list_pdc
+	 *            necessaire pour la courbe
+	 * @param t
+	 *            = le pas
+	 * @return un point de la courbe à l'instant t
+	 */
+	private Point createCurve(List<Point> list_pdc, float t) {
+		ArrayList<Point> list_pv = new ArrayList<Point>();
+		if (list_pdc.size() - 1 == 0) {
+			return list_pdc.get(0);
+		}
+		for (int i = 0; i < list_pdc.size() - 1; i++) {
+			int x = (int) ((1 - t) * list_pdc.get(i).getX() + t
+					* list_pdc.get(i + 1).getX());
+			int y = (int) ((1 - t) * list_pdc.get(i).getY() + t
+					* list_pdc.get(i + 1).getY());
+			list_pv.add(new Point(x, y));
+		}
+		return createCurve(list_pv, t);
+	}
 
-  /**
-   * @see Modele
-   */
-  @Override
-  public void reset() {
-    createPointCtrl();
-    obstacles = new ArrayList<Obstacle>();
-    genObstacles();
-    courbe = new ArrayList<>();
-  }
+	/**
+	 * Fonction qui genere les obstacles
+	 */
+	void genObstacles() {
+		super.genObstacles();
+	}
+
+	/**
+	 * @see Modele
+	 */
+	@Override
+	public void reset() {
+		createPointCtrl();
+		obstacles = new ArrayList<Obstacle>();
+		genObstacles();
+		courbe = new ArrayList<>();
+	}
 
 }
---
+++ src/technique/Obstacle.java
@@ -0,0 +1,67 @@
+package technique;
+
+import java.awt.Color;
+import java.awt.Point;
+
+@SuppressWarnings("serial")
+public class Obstacle extends Point {
+
+  private boolean touched;
+  private Color c = Color.red;
+
+  /**
+   * Construit un obstacle
+   * 
+   * @param x coordonnee x de l'obstacle
+   * @param y coordonnee y de l'obstacle
+   */
+  public Obstacle(int x, int y) {
+    super(x, y);
+    this.touched = false;
+  }
+
+  /**
+   * 
+   * @return si l'obstacle est touche ou non
+   */
+  public boolean isTouched() {
+    return touched;
+  }
+
+  /**
+   * 
+   * @param touched si l'obstacle est touche ou non
+   */
+  public void setTouched(boolean touched) {
+    this.touched = touched;
+    if (touched) {
+      c = Color.YELLOW;
+    } else {
+      c = Color.red;
+    }
+  }
+
+  /**
+   * @see Point 
+   */
+  @Override
+  public double getX() {
+    return super.getX();
+  }
+
+  /**
+   * @see Point 
+   */
+  @Override
+  public double getY() {
+    return super.getY();
+  }
+
+  /**
+   * 
+   * @return la couleur de l'obstacle
+   */
+  public Color getC() {
+    return c;
+  }
+}

892dba22c0b4a8ee7b36c0564eadbe98249acbcd
Laurent Poulin <laurent.poulin420@gmail.com>
2015-11-30 10:55:14 +0100

    fin tests becs

--- src/tests/BecTest.java
+++ src/tests/BecTest.java
@@ -1,5 +1,6 @@
 package tests;
-
+import modele.*;
+import java.awt.Point;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +8,23 @@ import org.junit.Test;
 public class BecTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetPointeX() {
+		Bec testBec = new Bec(new Point(10,10),new Point(10,15));
+		assertEquals(10.00, testBec.getPointe().getX(),00.00);
+	}
+	
+	@Test
+	public void testGetPointeY() {
+		Bec testBec = new Bec(new Point(10,10),new Point(10,15));
+		assertEquals(15.00, testBec.getPointe().getY(),00.00);
+	}
+	
+	@Test
+	public void testSetPointe() {
+		Bec testBec = new Bec(new Point(10,10),new Point(10,15));
+		testBec.setPointe(new Point(15,20));
+		assertEquals(15.00, testBec.getPointe().getX(),00.00);
+		assertEquals(20.00, testBec.getPointe().getY(),00.00);
 	}
 
 }

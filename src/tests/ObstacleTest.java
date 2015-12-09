53576caefc609c4229a7bb7107d51ec7d3ec8a87
Laurent Poulin <laurent.poulin420@gmail.com>
2015-12-09 11:19:37 +0100

    test obstacles

--- src/tests/ObstacleTest.java
+++ src/tests/ObstacleTest.java
@@ -2,13 +2,55 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
+import modele.Obstacle;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ObstacleTest {
+	Obstacle O;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		O = new Obstacle(50, 55, 10);
 	}
 
+	@Test
+	public void testGetSize() {
+		assertEquals(10, O.getSize());
+	}
+	
+	@Test
+	public void testGetY() {
+		assertEquals(55, O.getY(),0);
+	}
+	
+	@Test
+	public void testGetC() {
+		if(!O.getC().equals(Color.RED)){
+			fail("Wrong color");
+		}
+	}
+
+	@Test
+	public void testGetX() {
+		assertEquals(50, O.getX(),0);
+	}
+	
+	@Test
+	public void testTouched() {
+		O.setTouched(false);
+		assertFalse(O.isTouched());
+	}
+	
+	
+	@Test
+	public void testMove() {
+		O.move(20, 20);
+		if(O.getX() != 20 || O.getY() != 20){
+			fail("Move don't work");
+		}
+	}
 }

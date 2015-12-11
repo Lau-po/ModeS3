443333f777c2f00de5ca65aa427d3f1f685a75ea
Laurent Poulin <laurent.poulin420@gmail.com>
2015-12-11 15:29:43 +0100

    test added

--- src/tests/BezierTest.java
+++ src/tests/BezierTest.java
@@ -2,13 +2,50 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.Bezier;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BezierTest {
+	Bezier bezier;
 
+	/**
+	 * Setting up tests
+	 */
+	@Before
+	public void setUp() {
+		bezier = new Bezier();
+	}
+
+	/**
+	 * Vérification du remplissage ds valeurs
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGo() {
+		bezier.go();
+		if (bezier.getCourbe().isEmpty() && bezier.getObstacles().isEmpty()) {
+			fail("Liste non renseignées");
+		}
+	}
+
+	/**
+	 * Vérification du reset des valeurs
+	 */
+	@Test
+	public void testReset() {
+		bezier = new Bezier();
+		bezier.go();
+		bezier.reset();
+
+		if (!bezier.getCourbe().isEmpty() && !bezier.getObstacles().isEmpty()) {
+			fail("Les listes ne sont pas remise à 0");
+		}
+
 	}
 
 }

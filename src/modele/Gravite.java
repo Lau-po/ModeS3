2a188ed1673c846c1d0a6420d822c0ff6c567ebc
dusartc <dusart.clement@gmail.com>
2015-12-13 17:22:30 +0100

    doc

--- src/modele/Gravite.java
+++ src/modele/Gravite.java
@@ -6,17 +6,26 @@ import java.util.ArrayList;
 
 public class Gravite extends ObservableModele {
 
+  /** Constate gravitationnel */
   private double g = 9.81;
+  /** pas de la simulation */
   private double dt = 0.01;
+  /** vecteur position */
   private double[] position = new double[] {0.0, 0.0};
+  /** vecteur vitesse */
   private double[] vitesse = new double[] {50.0, 75.0};
 
+  /**
+   * Constructeur de base
+   */
   public Gravite() {
     super();
     genObstacles();
   }
 
-
+  /**
+   * @see Modele
+   */
   @Override
   public void go() {
     while (!collision && position[1] >= 0) {
@@ -28,15 +37,21 @@ public class Gravite extends ObservableModele {
     }
   }
 
+  /**
+   * @see Modele
+   */
   @Override
   public void reset() {
     courbe = new ArrayList<Point>();
     obstacles = new ArrayList<>();
     position = new double[] {0.0, 0.0};
-    vitesse = new double[] {50.0, 50.0};
+    vitesse = new double[] {50.0, 75.0};
     genObstacles();
   }
 
+  /**
+   * Calcul la vitesse de proche en proche
+   */
   private void acceleration() {
     double dx = vitesse[0];
     double dz = vitesse[1];
@@ -45,6 +60,10 @@ public class Gravite extends ObservableModele {
     vitesse[1] = dz;
   }
 
+
+  /**
+   * Calcul la position de proche en proche
+   */
   private void deplacement() {
     double x = position[0];
     double z = position[1];

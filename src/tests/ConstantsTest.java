a7f27d8221a813cad002f8cbbdc29a559639a7d1
DrClockwork <obled.joel@gmail.com>
2015-12-13 15:27:02 +0100

    Javadoc

--- src/tests/ConstantsTest.java
+++ src/tests/ConstantsTest.java
@@ -7,13 +7,25 @@ import org.junit.Test;
 
 import resources.Constants;
 
+/**
+ * Classe de test sur les constantes
+ * 
+ * @author Groupe N5
+ */
 public class ConstantsTest {
 	Constants C;
+	
+	/**
+	 * Mise en place du test
+	 */
 	@Before
 	public void setUp(){
 		C = new Constants();
 	}
 
+	/**
+	 * Test sur le fichier prop
+	 */
 	@Test
 	public void Contantstest() {
 		if(Constants.NBR_SIMULATION != 10){
@@ -22,7 +34,7 @@ public class ConstantsTest {
 		if(Constants.PAS != 0.001){
 			fail("PAS != in prop file");
 		}
-		// Nouvelles constantes à venir fail du test en attendant//
+		// Nouvelles constantes Ã  venir fail du test en attendant//
 		fail("Not all constants are added yet!");
 	}
 

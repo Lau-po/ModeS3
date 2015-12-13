0e990b9a769d0bf6f9c3580c37c0fbfccdf70b9a
DrClockwork <obled.joel@gmail.com>
2015-12-13 15:29:22 +0100

    Javadoc

--- src/resources/Constants.java
+++ src/resources/Constants.java
@@ -3,11 +3,18 @@ package resources;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Classe qui stock les constantes nécessaires au fonctionnement du jeu.
+ * 
+ * @author Groupe N5
+ */
+
 public class Constants {
      
-     private static Properties props;
-
-     static {
+	/** Objet propriété **/
+    private static Properties props;
+    
+    static {
           props = new Properties();
           try {
                props.load(Constants.class.getClassLoader().getResourceAsStream("resources/constant.prop"));
@@ -16,20 +23,23 @@ public class Constants {
           }
      }
      
-     /*
+     /**
       * BEZIER.JAVA PROPERTIES
-      */    
+      **/    
      
      public static final double PAS = Double.parseDouble(props.getProperty("pas"));
      
-     /*
+     /**
       * MAIN.JAVA PROPERTIES
-      */ 
+      **/ 
      
      public static final int NBR_SIMULATION = Integer.parseInt(props.getProperty("nbrSimul"));
      
+     /**
+      * Fonction main pour la simulation
+      * @param args
+      */
      public static void main(String[] args) {
 		System.out.println(PAS + " " + NBR_SIMULATION);
 	}
-     
 }
